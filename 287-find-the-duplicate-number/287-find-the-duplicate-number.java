7b59b9b35614eac27be3e03779755acaab093828
rkc422401 <66917360+rkc422401@users.noreply.github.com>
2022-07-06 23:53:22 +0530

    Time: 6 ms (71.18%), Space: 75 MB (78.11%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -3,11 +3,12 @@ class Solution {
         //this takes the concept of the linke dlist cycle 
         int slow=nums[0];
         int fast=nums[0];
-        
-        do{
+        slow=nums[slow];
+        fast=nums[nums[fast]];
+        while(slow !=fast){
             slow=nums[slow];
             fast=nums[nums[fast]];
-        }while(slow !=fast);
+        }
         
         fast=nums[0];
         while(slow !=fast){
