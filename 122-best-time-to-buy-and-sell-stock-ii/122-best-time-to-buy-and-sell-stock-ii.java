cf551e9b6778a2337fa0f322249c80b3d9fb66b8
rkc422401 <66917360+rkc422401@users.noreply.github.com>
2022-07-07 19:03:55 +0530

    Time: 1 ms (97.19%), Space: 43.7 MB (79.78%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        //max profit kitna ban sakta hai 
+        //transaction is unlimited 
+          int ans=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i] >= prices[i-1]) ans+=prices[i]-prices[i-1];
+        }
+        return ans;
+    }
+}
+// store =4+3=7
+//    7 1 5 3 6 4
+//         5-1=4   
+//             6-3=3
+              
