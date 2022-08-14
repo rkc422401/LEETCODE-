27ea0bd8d9c9ca6f6680bf22c540fa13208a284b
rkc422401 <66917360+rkc422401@users.noreply.github.com>
2022-08-14 14:15:03 +0530

    Time: 1 ms (98.27%), Space: 44.6 MB (63.74%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        //step 1: to make the answere list 
+        List<List<Integer>> ans=new ArrayList();
+        helper(0,nums,ans);
+        return ans;
+    }
+    //step 2: to make the helper methord
+    void helper(int index,int[] nums,List<List<Integer>> ans){
+        //now base condition 
+        if(index==nums.length){
+            List<Integer> temp=new  ArrayList();
+            for(int i=0;i<nums.length;i++){
+                temp.add(nums[i]);
+            }
+            ans.add(temp);
+        }
+        
+        //now visit every index from index 0 to nums.length
+        for(int i=index;i<nums.length;i++){
+            //now swap the index and the i
+            swap(index,i,nums);
+            helper(index+1,nums,ans);
+            swap(index,i,nums);      //reswap for the backtracking 
+        }
+    }
+    void swap(int a,int b,int[] nums){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
+}
