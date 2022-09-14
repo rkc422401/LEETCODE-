25fa9f942cae2363eb07544d52f1489903cd1d12
rkc422401 <66917360+rkc422401@users.noreply.github.com>
2022-09-14 12:09:57 +0530

    Time: 1 ms (98.72%), Space: 40.6 MB (93.39%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,17 +1,15 @@
+
 class Solution {
     public int numDecodings(String s) {
-        //if(s == null || s.length()==0) return 0;
         int[] dp=new int[s.length()+1];
         dp[0]=1;
-        dp[1]=s.charAt(0) == '0' ? 0:1;
-        for(int i=2;i<=s.length();i++){
+        dp[1]=s.charAt(0)=='0' ? 0:1;
+        for(int i=2;i<dp.length;i++){
             int first=Integer.valueOf(s.substring(i-1,i));
+            int sec=Integer.valueOf(s.substring(i-2,i));
             
-            int secound=Integer.valueOf(s.substring(i-2,i));
-            
-            if(first>=1 && first<=9) dp[i] +=dp[i-1];
-            
-            if(secound>=10 && secound<=26) dp[i] +=dp[i-2];
+            if(first>=1 && first<=9) dp[i]+=dp[i-1];
+            if(sec>=10 && sec<=26) dp[i]+=dp[i-2];
         }
         return dp[s.length()];
     }
