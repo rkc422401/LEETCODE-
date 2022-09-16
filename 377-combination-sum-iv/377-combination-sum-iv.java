e2281155e1ef77b0696a03aa109ed2c05959ad7d
rkc422401 <66917360+rkc422401@users.noreply.github.com>
2022-09-16 10:59:43 +0530

    Time: 3 ms (29.64%), Space: 41.5 MB (43.90%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,8 +1,6 @@
 class Solution {
     public int combinationSum4(int[] nums, int target) {
-        //changing varibles ke dop
         int[] dp=new int[target+1];
-        //Arrays.fill(dp,-1);
         dp[0]=1;
         for(int i=1;i<=target;i++){
             for(int n:nums){
